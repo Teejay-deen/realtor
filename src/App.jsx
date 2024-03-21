415c3a1c3e2a826fde38a7833264a96d333e21bc
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-03-21 12:03:47 +0100

    Adding pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Navbar from './pages/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Blog from "./pages/Blog/Blog"
+import Faq from "./pages/FAQ/Faq"
+import BecomeAgent from './pages/Become an Agent/BecomeAgent'
+import ContactUs from "./pages/Contact Us/ContactUs"
+
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/faq' element={<Faq />} />
+        <Route path='/BecomeAgent' element={<BecomeAgent />} />
+        <Route path='/Contact Us' element= {<ContactUs />} />
+
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Become an Agent/BecomeAgent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./BecomeAgent.css"
+
+const BecomeAgent = () => {
+  return (
+    <div>BecomeAgent</div>
+  )
+}
+
+export default BecomeAgent
