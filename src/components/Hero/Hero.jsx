0693dbc6ca87580c3d143149ea98f205095ec4ae
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-03-22 08:46:29 +0100

    adding drop down box

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,14 +19,50 @@ const Hero = () => {
           <button>Hotel Booking</button>
         </div>
 
-        <div className="search">
-         <label>Looking for</label>
-         <label>Location</label>
-         <label>Property size</label>
-         <label>Budget</label>
+        <div className="searchbar">
+          <div className="looking">
+            <label>Looking for</label>
+            <select name="sale" id="Looking">
+              <option value="Sale">Sale</option>
+              <option value="Rent">Rent</option>
+            </select>
+          </div>
 
+          <div className="location">
+            <label>Location</label>
+            <select className="location" name="location" id="location">
+              <option value="any">any</option>
+              <option value="Kwara">Kwara</option>
+              <option value="Abuja">Abuja</option>
+              <option value="Lagos">Lagos</option>
+              <option value="Ibadan">Ibadan</option>
+              <option value="Niger">Niger</option>
+            </select>
+          </div>
 
-         <button>Search</button>
+          <div className="propertySize">
+            <label>Property size</label>
+            <select name="propertySize" id="property">
+              <option value="house">House</option>
+              <option value="apartment">Apartment</option>
+            </select>
+          </div>
+
+          <div className="budget">
+          <label>Budget</label>
+            <select name="budget" id="budgets">
+              <option value="budget">Budgets</option>
+              <option value="saab">Mid-Range</option>
+              <option value="affordable">Affordable</option>
+              <option value="luxury">Luxury</option>
+            </select>
+          </div>
+        
+
+        
+          <div className="button">
+            <button>Search</button>
+          </div>
         </div>
       </div>
     </div>
