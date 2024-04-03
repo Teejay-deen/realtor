8f7c43ce731c72d58e3dad3a1ff9eef0f2388210
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-04-03 13:30:43 +0100

    make some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,28 @@ import Blog from "./pages/Blog/Blog";
 import Faq from "./pages/FAQ/Faq";
 import BecomeAgent from "./pages/Become an Agent/BecomeAgent";
 import ContactUs from "./pages/Contact Us/ContactUs";
+import { useState } from "react";
+import LoginPopUp from "./components/LoginPopUp/LoginPopUp";
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/become-agent" element={<BecomeAgent />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-      </Routes>
-    </div>
+    <>
+      {/* adding the tenary operator for loginpopup */}
+      {showLogin ? <LoginPopUp setShowLogin={setShowLogin} /> : <></>}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/become-agent" element={<BecomeAgent />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
--- src/components/LoginPopUp/LoginPopUp.jsx
+++ src/components/LoginPopUp/LoginPopUp.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./LoginPopUp.css"
+import { assets } from '../../assets/assets'
 
-const LoginPopUp = () => {
+const LoginPopUp = ({setShowLogin}) => {
+
+    const [currentState, setCurrentState] = useState("Sign Up")
   return (
-    <div>
-        
+    <div className='login-popup'>
+        <form action="" className="login-popup-container">
+            <div className="login-popup-title">
+                <h2>{currentState}</h2>
+                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+            </div>
+            <div className="login-popup-inputs">
+                <input type="text" placeholder='Your Name' required />
+                <input type="email" placeholder='Your Email' required />
+            </div>
+        </form>
     </div>
   )
 }
